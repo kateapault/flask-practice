148fbb5ac844b9ba7923a2dfc3c441529b08828d
github username <kateapault@gmail.com>
2020-09-30 16:35:51 -0400

    part 2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username':'Kate'}
+    posts = [
+        {
+            'author': {'username':'Lana'},
+            'body': 'The park is awesome'
+        },
+        {
+            'author': {'username':'Kate'},
+            'body': 'I need to concentrate on this more'
+        }
+    ]
+    return render_template('index.html',title="Home",user=user,posts=posts)
